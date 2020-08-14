843c45ae063e340f298f31c975e521386f8f4ed8
Alex Saveau <asaveau@google.com>
2020-08-14 01:05:33 +0000

    Add function summary component
    Fixes: 163923922
    Change-Id: I4fa0bf9cb85df8fd3ce4ba7f58b32a7d42657cb6

---
+++ src/main/java/com/google/devsite/components/impl/DefaultFunctionSummary.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devsite.components.impl
+
+import com.google.devsite.components.FunctionSummary
+import kotlinx.html.Entities
+import kotlinx.html.TR
+import kotlinx.html.code
+import kotlinx.html.div
+import kotlinx.html.span
+import kotlinx.html.td
+
+/**
+ * Default implementation of a function summary row.
+ *
+ * The return type and modifiers will be in the left column, while the signature and short
+ * description are on the right. Annotations should not be included in the summary to save space.
+ */
+internal class DefaultFunctionSummary(
+    override val data: FunctionSummary.Params
+) : FunctionSummary {
+    override fun render(html: TR) = html.run {
+        td {
+            for (modifier in data.modifiers) {
+                span("keyword") { +modifier }
+                +Entities.nbsp
+            }
+
+            data.returnType.render(this)
+        }
+
+        td {
+            div {
+                code {
+                    data.signature.render(this)
+                }
+            }
+
+            data.description.render(this)
+        }
+    }
+}
---
+++ src/test/java/com/google/devsite/components/impl/DefaultFunctionSummaryTest.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devsite.components.impl
+
+import com.google.common.truth.Truth.assertThat
+import com.google.devsite.components.FunctionSummary.Params
+import com.google.devsite.components.testing.NoopDocumentation
+import com.google.devsite.components.testing.NoopFunctionSignature
+import com.google.devsite.components.testing.NoopLink
+import kotlinx.html.stream.createHTML
+import kotlinx.html.table
+import kotlinx.html.tr
+import org.junit.Test
+
+class DefaultFunctionSummaryTest {
+    @Test
+    fun `Simple function summary renders correctly`() {
+        val component = DefaultFunctionSummary(
+            Params(
+                modifiers = emptyList(),
+                returnType = NoopLink("Unit"),
+                signature = NoopFunctionSignature("foo()"),
+                description = NoopDocumentation("This method does baz.")
+            )
+        )
+
+        val output = createHTML().table {
+            tr { component.render(this) }
+        }.trim()
+
+        // language=html
+        assertThat(output).isEqualTo(
+            """
+<table>
+  <tr>
+    <td>Unit</td>
+    <td>
+      <div><code>foo()</code></div>
+      <p>This method does baz.</p>
+    </td>
+  </tr>
+</table>
+            """.trim()
+        )
+    }
+
+    @Test
+    fun `Function summary with modifiers renders correctly`() {
+        val component = DefaultFunctionSummary(
+            Params(
+                modifiers = listOf("open", "suspend"),
+                returnType = NoopLink("Unit"),
+                signature = NoopFunctionSignature("foo()"),
+                description = NoopDocumentation("This method does baz.")
+            )
+        )
+
+        val output = createHTML().table {
+            tr { component.render(this) }
+        }.trim()
+
+        // language=html
+        assertThat(output).isEqualTo(
+            """
+<table>
+  <tr>
+    <td><span class="keyword">open</span>&nbsp;<span class="keyword">suspend</span>&nbsp;Unit</td>
+    <td>
+      <div><code>foo()</code></div>
+      <p>This method does baz.</p>
+    </td>
+  </tr>
+</table>
+            """.trim()
+        )
+    }
+}
