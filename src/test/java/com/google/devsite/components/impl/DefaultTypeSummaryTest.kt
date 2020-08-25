cd00c182c7569d4f0ae1bf6e67bb3ea1298f2e50
Alex Saveau <asaveau@google.com>
2020-08-25 02:52:55 +0000

    Add type summary component
    Bug: 165811690
    Change-Id: I5f046e24e9acc653bfb955896779ef76b7ef113e

---
+++ src/test/java/com/google/devsite/components/impl/DefaultTypeSummaryTest.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devsite.components.impl
+
+import com.google.common.truth.Truth.assertThat
+import com.google.devsite.components.TypeSummary
+import com.google.devsite.components.testing.NoopParameterType
+import kotlinx.html.div
+import kotlinx.html.stream.createHTML
+import org.junit.Test
+
+class DefaultTypeSummaryTest {
+    @Test
+    fun `Simple type renders correctly`() {
+        val component = DefaultTypeSummary(
+            TypeSummary.Params(
+                type = NoopParameterType("Unit")
+            )
+        )
+
+        val output = createHTML().div {
+            component.render(this)
+        }.trim()
+
+        // language=html
+        assertThat(output).isEqualTo(
+            """
+<div>Unit</div>
+            """.trim()
+        )
+    }
+
+    @Test
+    fun `Type with modifiers renders correctly`() {
+        val component = DefaultTypeSummary(
+            TypeSummary.Params(
+                modifiers = listOf("open", "suspend"),
+                type = NoopParameterType("Unit")
+            )
+        )
+
+        val output = createHTML().div {
+            component.render(this)
+        }.trim()
+
+        // language=html
+        assertThat(output).isEqualTo(
+            """
+<div>open&nbsp;suspend&nbsp;Unit</div>
+            """.trim()
+        )
+    }
+}
