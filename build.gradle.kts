1904c19f9756bb63ba97123d0f5fe64bb849de1f
Andrea Falcone <asfalcone@google.com>
2020-09-03 16:17:33 -0400

    Update to Dokka 1.4.10-dev-64
    Replace missing classes with new ones
    Change tests and FunctionDocumentableConverter to handle Variance<*>
    Update integration test to handle new cli args
    
    Test: ./gradlew
    BUG: 167601539
    Change-Id: Ia00c15ffc2a26cc4dfea10904cdbc7f6f4c4b455

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ application {
     mainClassName = "org.jetbrains.dokka.MainKt"
 }
 
-val dokkaVersion = "1.4.0-rc-24"
+val dokkaVersion = "1.4.10-dev-64"
 
 dependencies {
     compileOnly(kotlin("stdlib-jdk8"))
