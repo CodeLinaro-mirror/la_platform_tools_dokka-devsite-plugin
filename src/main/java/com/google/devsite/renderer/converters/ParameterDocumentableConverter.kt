b58360d26dc7f565ffe9424e06afd5d7a63a75b3
Alex Saveau <asaveau@google.com>
2020-09-09 22:07:39 +0000

    Extract parameter converter logic into its own class
    This will be used for the property converter.
    
    Change-Id: Ib64e44be283f9e5fc34d8d0d069398399420baed

---
+++ src/main/java/com/google/devsite/renderer/converters/ParameterDocumentableConverter.kt
@@ -0,0 +1,209 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devsite.renderer.converters
+
+import com.google.devsite.components.Link
+import com.google.devsite.components.Parameter
+import com.google.devsite.components.ParameterType
+import com.google.devsite.components.impl.DefaultLink
+import com.google.devsite.components.impl.DefaultParameter
+import com.google.devsite.components.impl.DefaultParameterType
+import com.google.devsite.renderer.Language
+import com.google.devsite.renderer.impl.paths.FilePathProvider
+import org.jetbrains.dokka.links.DRI
+import org.jetbrains.dokka.model.DParameter
+import org.jetbrains.dokka.model.FunctionModifiers
+import org.jetbrains.dokka.model.JavaObject
+import org.jetbrains.dokka.model.Nullable
+import org.jetbrains.dokka.model.PrimitiveJavaType
+import org.jetbrains.dokka.model.Projection
+import org.jetbrains.dokka.model.Star
+import org.jetbrains.dokka.model.TypeConstructor
+import org.jetbrains.dokka.model.TypeParameter
+import org.jetbrains.dokka.model.UnresolvedBound
+import org.jetbrains.dokka.model.Variance
+import org.jetbrains.dokka.model.Void
+
+/** Converts parameter and parameter-likes into their components. */
+internal class ParameterDocumentableConverter(
+    private val displayLanguage: Language,
+    private val pathProvider: FilePathProvider
+) {
+    /** Returns the component for a parameter. */
+    fun componentForParameter(param: DParameter): Parameter = when (displayLanguage) {
+        Language.JAVA -> componentForJavaProjection(param.type, param.name ?: "receiver")
+        Language.KOTLIN -> componentForKotlinProjection(param.type, param.name.orEmpty())
+    }
+
+    /** Returns the component for a type projection. */
+    fun componentForProjection(proj: Projection): Parameter = when (displayLanguage) {
+        Language.JAVA -> componentForJavaProjection(proj)
+        Language.KOTLIN -> componentForKotlinProjection(proj)
+    }
+
+    private fun componentForJavaProjection(proj: Projection, name: String = ""): Parameter {
+        return DefaultParameter(
+            Parameter.Params(
+                isLambda = false,
+                name = name,
+                primary = proj.toComponent(),
+                // TODO(b/165104993): figure out path to implementing annotations
+                annotations = emptyList(),
+                displayLanguage = Language.JAVA
+            )
+        )
+    }
+
+    private fun componentForKotlinProjection(proj: Projection, name: String = ""): Parameter {
+        val isLambda = proj.isLambda()
+
+        val receiver = proj.receiver()
+        val primaryType = if (isLambda) {
+            // Get the return type of the lambda
+            (proj as TypeConstructor).projections.last().toComponent()
+        } else {
+            proj.toComponent()
+        }
+        val lambdaModifiers: List<String> = if (proj.isLambda(suspendOnly = true)) {
+            listOf("suspend")
+        } else {
+            emptyList()
+        }
+        val lambdaParams: List<ParameterType> = if (isLambda) {
+            // Always ignore the return type of the lambda since that's handled by primaryType.
+            val lambdaProjections = (proj as TypeConstructor).projections.dropLast(1)
+            if (receiver == null) {
+                lambdaProjections.map { it.toComponent() }
+            } else {
+                // If the receiver is available, we also ignore the first type
+                lambdaProjections.drop(1).map { it.toComponent() }
+            }
+        } else {
+            emptyList()
+        }
+
+        return DefaultParameter(
+            Parameter.Params(
+                isLambda = isLambda,
+                name = name,
+                receiver = receiver,
+                lambdaModifiers = lambdaModifiers,
+                lambdaParams = lambdaParams,
+                primary = primaryType,
+                // TODO(b/165104993): figure out path to implementing annotations
+                annotations = emptyList(),
+                displayLanguage = Language.KOTLIN
+            )
+        )
+    }
+
+    /** Converts a lambda receiver projection to its type component if available. */
+    private fun Projection.receiver(): ParameterType? = when (this) {
+        is TypeConstructor -> if (modifier == FunctionModifiers.EXTENSION) {
+            projections.first().toComponent()
+        } else {
+            null
+        }
+        is TypeParameter, is PrimitiveJavaType, is UnresolvedBound, Star, JavaObject, Void -> null
+        is Nullable -> inner.receiver()
+        is Variance<*> -> inner.receiver()
+        else -> error("Unknown bound: $this")
+    }
+
+    /** Converts a documentable type to its type component, recursively expanding generics */
+    private fun Projection.toComponent(): ParameterType {
+        if (this is Variance<*>) {
+            return inner.toComponent()
+        }
+
+        val generics: List<ParameterType> = when (this) {
+            is TypeConstructor -> projections.map { it.toComponent() }
+            is TypeParameter, is PrimitiveJavaType, is UnresolvedBound,
+            Star, Void, JavaObject -> emptyList()
+            is Nullable -> listOf(inner.toComponent())
+            // TODO(b/166530498): support variance
+            is Variance<*> -> listOf(inner.toComponent())
+            else -> error("Unknown bound: $this")
+        }
+
+        return DefaultParameterType(
+            ParameterType.Params(
+                type = toLink(),
+                generics = generics
+            )
+        )
+    }
+
+    /**
+     * Converts a documentable type to a link component, assuming all generics have been resolved.
+     */
+    private fun Projection.toLink(): Link = when (this) {
+        is TypeConstructor -> pathProvider.linkForReference(dri)
+        is TypeParameter -> DefaultLink(
+            Link.Params(
+                name = name,
+                url = ""
+            )
+        )
+        Star -> DefaultLink(
+            Link.Params(
+                name = when (displayLanguage) {
+                    Language.JAVA -> "?"
+                    Language.KOTLIN -> "*"
+                },
+                url = ""
+            )
+        )
+        Void -> when (displayLanguage) {
+            Language.JAVA -> DefaultLink(Link.Params(name = "void", url = ""))
+            Language.KOTLIN -> pathProvider.linkForReference(DRI("kotlin", "Unit"))
+        }
+        JavaObject -> when (displayLanguage) {
+            Language.JAVA -> pathProvider.linkForReference(DRI("java.lang", "Object"))
+            Language.KOTLIN -> pathProvider.linkForReference(DRI("kotlin", "Any"))
+        }
+        is PrimitiveJavaType -> when (displayLanguage) {
+            Language.JAVA -> DefaultLink(Link.Params(name = name, url = ""))
+            Language.KOTLIN -> pathProvider.linkForReference(DRI("kotlin", name.capitalize()))
+        }
+        is UnresolvedBound -> DefaultLink(Link.Params(name = name, url = ""))
+        is Nullable -> inner.toLink()
+        // TODO(b/166530498): support variance
+        is Variance<*> -> inner.toLink()
+        else -> error("Unknown bound: $this")
+    }
+
+    /** Determine whether or not a param is a lambda using the kotlin function type. */
+    private fun Projection.isLambda(suspendOnly: Boolean = false): Boolean = when (this) {
+        is TypeConstructor -> {
+            val typeName = dri.classNames.orEmpty()
+            val isStandardLambda = dri.packageName == "kotlin" && typeName.startsWith("Function")
+            val isSuspendLambda =
+                dri.packageName == "kotlin.coroutines" && typeName.startsWith("SuspendFunction")
+
+            if (suspendOnly) {
+                isSuspendLambda
+            } else {
+                isStandardLambda || isSuspendLambda
+            }
+        }
+        is Nullable -> inner.isLambda()
+        is Variance<*> -> inner.isLambda()
+        is TypeParameter, is PrimitiveJavaType, is UnresolvedBound, Star, JavaObject, Void -> false
+        else -> error("Unknown bound: $this")
+    }
+}
---
+++ src/test/java/com/google/devsite/renderer/converters/ParameterDocumentableConverterTest.kt
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devsite.renderer.converters
+
+import com.google.common.truth.Truth.assertThat
+import com.google.devsite.components.Link
+import com.google.devsite.components.Parameter
+import com.google.devsite.components.ParameterType
+import com.google.devsite.renderer.Language
+import com.google.devsite.renderer.converters.testing.item
+import com.google.devsite.renderer.converters.testing.items
+import com.google.devsite.testing.ConverterTestBase
+import org.jetbrains.dokka.model.DModule
+import org.jetbrains.dokka.model.DParameter
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+internal class ParameterDocumentableConverterTest(
+    private val language: Language
+) : ConverterTestBase(language) {
+    @Test
+    fun `Parameter has correct type`() {
+        val param = """
+            |fun foo(a: String) = Unit
+        """.render().param()
+
+        val paramType = param.data.primary
+
+        assertThat(paramType.link().name).isEqualTo("String")
+    }
+
+    @Test
+    fun `Parameter understands generics`() {
+        val param = """
+            |fun foo(a: List<Int>)
+        """.render().param()
+
+        val paramType = param.data.primary
+        val generic = paramType.data.generics.item()
+
+        assertThat(generic.link().name).isEqualTo("Int")
+    }
+
+    @Test
+    fun `Parameter understands nested generics`() {
+        val param = """
+            |fun foo(a: List<Set<Int>>)
+        """.render().param()
+
+        val paramType = param.data.primary
+        val generic = paramType.data.generics.item()
+        val nestedGeneric = generic.data.generics.item()
+
+        assertThat(nestedGeneric.link().name).isEqualTo("Int")
+    }
+
+    @Test
+    fun `Parameter understands * generics`() {
+        val param = """
+            |fun foo(a: List<*>)
+        """.render().param()
+
+        val paramType = param.data.primary
+        val generic = paramType.data.generics.item()
+
+        javaOnly { assertThat(generic.link().name).isEqualTo("?") }
+        kotlinOnly { assertThat(generic.link().name).isEqualTo("*") }
+    }
+
+    @Test
+    fun `Parameter understands lambda with difficult generic combination`() {
+        val param = """
+            |fun foo(block: Int.(Map<String, Int>, Double) -> Collection<Float>)
+        """.render().param().data
+
+        javaOnly {
+            assertNoLambdaStuff(param)
+
+            val primary = param.primary.data
+            assertThat(primary.type.data.name).isEqualTo("Function3")
+            assertThat(primary.generics).hasSize(4)
+            assertThat(primary.generics[0].link().name).isEqualTo("Int")
+            assertThat(primary.generics[1].link().name).isEqualTo("Map")
+            assertThat(primary.generics[2].link().name).isEqualTo("Double")
+            assertThat(primary.generics[3].link().name).isEqualTo("Collection")
+
+            val mapGenerics = primary.generics[1].data.generics.items(2)
+            assertThat(mapGenerics.first().link().name).isEqualTo("String")
+            assertThat(mapGenerics.last().link().name).isEqualTo("Int")
+
+            val collectionGeneric = primary.generics[3].data.generics.item()
+            assertThat(collectionGeneric.link().name).isEqualTo("Float")
+        }
+        kotlinOnly {
+            assertThat(param.isLambda).isTrue()
+            assertThat(param.receiver).isNotNull()
+            assertThat(param.receiver!!.link().name).isEqualTo("Int")
+            assertThat(param.lambdaModifiers).isEmpty()
+            assertThat(param.lambdaParams).hasSize(2)
+            assertThat(param.lambdaParams.first().link().name).isEqualTo("Map")
+            assertThat(param.lambdaParams.last().link().name).isEqualTo("Double")
+            assertThat(param.primary.link().name).isEqualTo("Collection")
+
+            val mapGenerics = param.lambdaParams.first().data.generics.items(2)
+            assertThat(mapGenerics.first().link().name).isEqualTo("String")
+            assertThat(mapGenerics.last().link().name).isEqualTo("Int")
+
+            val collectionGeneric = param.primary.data.generics.item()
+            assertThat(collectionGeneric.link().name).isEqualTo("Float")
+        }
+    }
+
+    private fun DModule.param(): Parameter {
+        val converter = ParameterDocumentableConverter(language, pathProvider())
+        return converter.componentForParameter(parameterDoc())
+    }
+
+    private fun DModule.parameterDoc(): DParameter {
+        return packages.single().functions.single().parameters.single()
+    }
+
+    private fun ParameterType.link(): Link.Params = data.type.data
+
+    private fun assertNoLambdaStuff(data: Parameter.Params) {
+        assertThat(data.isLambda).isFalse()
+        assertThat(data.receiver).isNull()
+        assertThat(data.lambdaParams).isEmpty()
+        assertThat(data.lambdaModifiers).isEmpty()
+    }
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "{0}")
+        fun data() = listOf(
+            arrayOf(Language.JAVA),
+            arrayOf(Language.KOTLIN)
+        )
+    }
+}
