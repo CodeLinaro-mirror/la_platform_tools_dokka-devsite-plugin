2256aa31e6217d66f3051178738b98f079d86465
Alex Saveau <asaveau@google.com>
2020-09-10 22:29:51 +0000

    Add paging to integ test update script
    Change-Id: I63f0760727b206b50cbd166c7937d2ced6b543e6

--- update-all-integ-tests.sh
+++ update-all-integ-tests.sh
@@ -4,5 +4,6 @@
 
 rm -rf testData/fragment/docs && cp -r build/docs/testData/fragment/docs testData/fragment/docs
 rm -rf testData/innerClasses/docs && cp -r build/docs/testData/innerClasses/docs testData/innerClasses/docs
+rm -rf testData/paging/docs && cp -r build/docs/testData/paging/docs testData/paging/docs
 rm -rf testData/simple/docs && cp -r build/docs/testData/simple/docs testData/simple/docs
 rm -rf testData/topLevelFunctions/docs && cp -r build/docs/testData/topLevelFunctions/docs testData/topLevelFunctions/docs
