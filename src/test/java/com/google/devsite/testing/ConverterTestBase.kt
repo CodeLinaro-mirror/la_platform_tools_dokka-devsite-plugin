806d692cc09ebc9d7edf5d8e2378b7651a80479a
Andrea Falcone <asfalcone@google.com>
2020-11-14 17:28:13 -0500

    Add stdlib to class path for tests
    This resolves instances of <ERROR CLASS> (Coroutine scope, certain annotations, etc)
    in integration and unit tests
    But will need to be handled by callers in invocation of Dackka
    
    
    BUG: 168667502
    BUG: 170179190
    
    Change-Id: I53888962429a8feea31624214019c09b9cdcd19b

--- src/test/java/com/google/devsite/testing/ConverterTestBase.kt
+++ src/test/java/com/google/devsite/testing/ConverterTestBase.kt
@@ -72,6 +72,7 @@ internal abstract class ConverterTestBase(
             sourceSets {
                 sourceSet {
                     sourceRoots = listOf("src/main")
+                    classpath += listOfNotNull(jvmStdlibPath)
                 }
             }
         }
