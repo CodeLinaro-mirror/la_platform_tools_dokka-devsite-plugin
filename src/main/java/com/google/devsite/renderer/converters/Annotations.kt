1b504b7b697240a89aa4c1979f96356d6ad19765
Tiem Song <tiem@google.com>
2021-05-21 12:30:56 -0700

    Ignore @CheckResult annotation
    Bug: 188090261
    Test: ./gradlew test
    Change-Id: Ia762db03762c9e4dbc8b344921161683089a75f7

--- src/main/java/com/google/devsite/renderer/converters/Annotations.kt
+++ src/main/java/com/google/devsite/renderer/converters/Annotations.kt
@@ -98,12 +98,15 @@ private fun shouldDocumentAnnotation(annotation: Annotation, language: Language)
     // Not useful to developers
     val isSuppressAnnotation = annotation.dri.classNames in SUPPRESSION_ANNOTATION_NAMES
     val isKotlinJvmAnnotation = annotation.dri.packageName == "kotlin.jvm"
+    val isCheckResultAnnotation = annotation.dri.classNames == "CheckResult"
+
     // Surfaced separately
     val isDeprecatedAnnotation = annotation.isDeprecated()
     val isNullabilityAnnotation = annotation.dri.classNames in NULLABILITY_ANNOTATION_NAMES
 
     return !isSuppressAnnotation &&
         !isKotlinJvmAnnotation &&
+        !isCheckResultAnnotation &&
         !isDeprecatedAnnotation &&
         // Keep nullability annotations for Java
         (language == Language.JAVA || !isNullabilityAnnotation)
--- src/test/java/com/google/devsite/renderer/converters/AnnotationsTest.kt
+++ src/test/java/com/google/devsite/renderer/converters/AnnotationsTest.kt
@@ -58,6 +58,20 @@ internal class AnnotationsTest : ConverterTestBase() {
     }
 
     @Test
+    fun `@CheckResult annotation is ignored`() {
+
+        // Declare the CheckResult annotation class before it's used
+        val annotation = """
+            |annotation class CheckResult
+            |
+            |@CheckResult
+            |fun foo() = Unit
+        """.render().functionAnnotations()
+
+        assertThat(annotation.components().exceptNonNull()).isEmpty()
+    }
+
+    @Test
     fun `@Deprecated annotations are ignored since they are surfaced separately`() {
         val annotations = """
             |@Deprecated("So long, farewell, auf wiedersehen, goodbye")
