2481c6469ec9a2325a217e23e2d714310e765107
Andrea Falcone <asfalcone@google.com>
2021-09-15 13:18:47 +0000

    Update Dackka to 0.0.10
    Updates to Dokka 1.5.30-dev-118
    * better handling of <pre> and @code
    * javadoc parsing improvements like allow <img> and handle @link split over lines
    Fixes annotation on Classlike signatures
    Improved error messages for invalid @param
    
    Change-Id: I0bf7d66e06ba16d35ca45707bea6bf60c4f71774

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
 }
 
 group = "com.google.devsite"
-version = "0.0.9" // This is appended to archiveBaseName in the ShadowJar task.
+version = "0.0.10" // This is appended to archiveBaseName in the ShadowJar task.
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
