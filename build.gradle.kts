6a87c8425d1fd845cb468f388ea66b6ac8e4b3ee
Owen Gray <owengray@google.com>
2022-07-11 14:43:51 +0000

    Bump dackka version to 0.0.19
    Change-Id: Ie08d72b8b20d1d485d650a531a39b55fa5026b74
    Test: N/A

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
 }
 
 group = "com.google.devsite"
-version = "0.0.18" // This is appended to archiveBaseName in the ShadowJar task.
+version = "0.0.19" // This is appended to archiveBaseName in the ShadowJar task.
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
