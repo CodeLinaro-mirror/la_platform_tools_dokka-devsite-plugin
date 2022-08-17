fad43126a5bf3dbd11c0fdf40819981dad46dd26
Owen Gray <owengray@google.com>
2022-08-17 10:26:28 -0400

    Bump dackka version to 1.0.0
    This is the version that will be deprecating old dokka and doclava
    (sans bugfixes)
    Test: N/A
    
    Change-Id: I2f26bae666cc4d466bcac0a93f78580a146893be

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
 }
 
 group = "com.google.devsite"
-version = "0.0.19" // This is appended to archiveBaseName in the ShadowJar task.
+version = "1.0.0" // This is appended to archiveBaseName in the ShadowJar task.
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
