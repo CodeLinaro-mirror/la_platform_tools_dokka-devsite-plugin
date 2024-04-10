1aad73a888fed357b6737efab6e3568ee2f65913
Aurimas Liutikas <aurimas@google.com>
2024-04-10 21:30:31 +0000

    Remove the usage of an old AGP alpha version
    Change-Id: Ie25b63e3757cd926b60778e54f7987db5673be7d

--- build.gradle.kts
+++ build.gradle.kts
@@ -155,7 +155,7 @@ dependencies {
     testDataImpl("org.robolectric:android-all-instrumented:13-robolectric-9030017-i4")
     testDataImpl("junit:junit:4.13.2")
     testDataImpl("com.google.truth:truth:1.1.3")
-    testDataImpl("com.android.tools.build:gradle:8.3.0-alpha10")
+    testDataImpl("com.android.tools.build:gradle:8.3.1")
 
     testDataImpl(fileTree("${layout.buildDirectory.get()}/exploded"))
 
