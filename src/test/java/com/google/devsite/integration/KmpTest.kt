7f082eb948e28753e4d84d42f7cc9a6f147c6a86
Owen Gray <owengray@google.com>
2025-03-05 14:12:22 -0500

    Add ability to run ToT dackka aginst docs-public KMP
    This is useful for benchmarking and validation purposes.
    
    Test: adds an @ignore's must-be-manually-run test
    Change-Id: I963c94d30a8cb4ac9babb9a2fd16586f8226bdcb

--- src/test/java/com/google/devsite/integration/KmpTest.kt
+++ src/test/java/com/google/devsite/integration/KmpTest.kt
@@ -18,6 +18,7 @@ package com.google.devsite.integration
 
 import com.google.devsite.capitalize
 import com.google.devsite.testing.IntegrationTestBase
+import com.google.devsite.testing.classpathFromFile
 import java.io.File
 import kotlin.test.Ignore
 import org.jetbrains.dokka.DokkaConfiguration
@@ -105,56 +106,94 @@ class KmpTest : IntegrationTestBase() {
         samplesLocations: List<String>,
         includeFiles: List<String>,
         externalLinks: List<ExternalDocumentationLinkImpl>,
-    ) {
-        fun SourceSetsBuilder.createSourceSet(
-            name: String,
-            sourcesOfPlatform: List<File>,
-            ssDependencies: Set<DokkaSourceSetID> = emptySet(),
-            displayName: String = name.capitalize(),
-            analysisPlatform: String = name,
-        ) = sourceSet {
-            this.name = name
-            this.displayName = displayName
-            samples = if ("ommon" in name) samplesLocations else emptyList()
-            sourceRoots = sourcesOfPlatform.map { it.absolutePath }
-            classpath = classpathFromFile("testData/classpath.txt")
-            externalDocumentationLinks = externalLinks
-            documentedVisibilities =
-                setOf(
-                    DokkaConfiguration.Visibility.PUBLIC,
-                    DokkaConfiguration.Visibility.PROTECTED,
-                )
-            this.analysisPlatform = analysisPlatform
-            this.dependentSourceSets = ssDependencies
-        }
-        fun List<File>.filterForPlatform(identifier: String) =
-            filter { it.isDirectory }
-                .flatMap {
-                    it.listFiles()?.filter { identifier.lowercase() in it.name.lowercase() }
-                        ?: emptyList()
-                }
+    ) =
+        multiPlatformSourceSets(
+            sources,
+            samplesLocations,
+            includeFiles,
+            externalLinks,
+            squashAndroid
+        )
+}
 
-        val sourceFolders =
-            listOf("jvm", "android", "native", "js")
-                .associateWith { sources.filterForPlatform(it) }
-                .toMutableMap()
-
-        if (squashAndroid) {
-            sourceFolders["jvm"] = sourceFolders["jvm"]!! + sourceFolders["android"]!!
-            sourceFolders.remove("android")
-        } else {
-            throw RuntimeException(
-                "Due to upstream squashing, not squashing android into jvm isn't currently " +
-                    "supported.",
+fun TestDokkaConfigurationBuilder.multiPlatformSourceSets(
+    rawSources: List<File>,
+    samplesLocations: List<String>,
+    includeFiles: List<String>,
+    externalLinks: List<ExternalDocumentationLinkImpl>,
+    squashAndroid: Boolean,
+) {
+    var sources = rawSources
+    fun SourceSetsBuilder.createSourceSet(
+        name: String,
+        sourcesOfPlatform: List<File>,
+        ssDependencies: Set<DokkaSourceSetID> = emptySet(),
+        displayName: String = name.capitalize(),
+        analysisPlatform: String = name,
+    ) = sourceSet {
+        this.name = name
+        this.displayName = displayName
+        samples = if ("ommon" in name) samplesLocations else emptyList()
+        sourceRoots = sourcesOfPlatform.map { it.absolutePath }
+        classpath = classpathFromFile("testData/classpath.txt")
+        externalDocumentationLinks = externalLinks
+        documentedVisibilities =
+            setOf(
+                DokkaConfiguration.Visibility.PUBLIC,
+                DokkaConfiguration.Visibility.PROTECTED,
             )
-        }
+        this.analysisPlatform = analysisPlatform
+        this.dependentSourceSets = ssDependencies
+    }
+    /**
+     * Called on a list of directories, returns the merged list of, for each directory:
+     * - If the directory contains **Main folders (e.g. jvmMain), returns the list of subfolders
+     *   whose names include the passed identifier.
+     * - If the directory doesn't contain **Main folders, the directory is presumably not KMP. Thus,
+     *   returns the same superfolder directory IFF the identifier is 'android', else nothing.
+     *
+     * All output folders should be of the filepath form e.g. androidx/package/subpackage/class.kt.
+     * Input KMP folders should be of the form e.g. source/commonMain/androidx/collection/Foo.kt
+     * Input non-KMP folders should be of the form e.g. source/dokkatest/simple/GenericInterface.kt
+     * The precise path is only truly important for java files, which parsing can be fragile.
+     * Existing integration tests and androidx unzippedXXXSources folders follow this pattern.
+     */
+    fun List<File>.filterForPlatform(identifier: String, ignoreCase: Boolean = true) =
+        filter { it.isDirectory }
+            .flatMap {
+                val mainSourceSetFolders = it.listFiles()!!.filter { it.path.endsWith("Main") }
+                // This allows androidx and android.support non-KMP sources as inputs
+                if (mainSourceSetFolders.isEmpty() && identifier == "android") listOf(it)
+                // standard KMP case for *Main sourceSets.
+                else mainSourceSetFolders.filter { it.name.contains(identifier, ignoreCase) }
+            }
 
-        return sourceSets {
-            val common = createSourceSet("common", sources.filterForPlatform("common"))
-            val dependOnCommon = setOf(common.value.sourceSetID)
-            sourceFolders
-                .filter { it.value.isNotEmpty() }
-                .map { createSourceSet(it.key, it.value, dependOnCommon) }
-        }
+    val sourceFolders =
+        listOf("jvm", "android", "native", "js")
+            .associateWith { sources.filterForPlatform(it) }
+            .toMutableMap()
+    // Aggravatingly, this filter sorts 'nonJvmMain' as a 'jvm' sourceSet. Explicitly fix.
+    val nonJvmSources = sources.filterForPlatform("nonJvm").toSet()
+    sourceFolders["jvm"] = sourceFolders["jvm"]!! - nonJvmSources
+    sourceFolders["android"] = sourceFolders["android"]!! - nonJvmSources
+    sourceFolders["native"] = sourceFolders["native"]!! + nonJvmSources
+    sourceFolders["js"] = sourceFolders["js"]!! + nonJvmSources
+
+    if (squashAndroid) {
+        sourceFolders["jvm"] = sourceFolders["jvm"]!! + sourceFolders["android"]!!
+        sourceFolders.remove("android")
+    } else {
+        throw RuntimeException(
+            "Due to upstream squashing, not squashing android into jvm isn't currently supported.",
+        )
+    }
+
+    return sourceSets {
+        val common =
+            createSourceSet("common", sources.filterForPlatform("commonMain", ignoreCase = false))
+        val dependOnCommon = setOf(common.value.sourceSetID)
+        sourceFolders
+            .filter { it.value.isNotEmpty() }
+            .map { createSourceSet(it.key, it.value, dependOnCommon) }
     }
 }
