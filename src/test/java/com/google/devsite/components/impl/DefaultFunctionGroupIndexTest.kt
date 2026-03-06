575f1c00763d30815fdb37e2c18f3f2736de2b24
Julia McClellan <juliamcclellan@google.com>
2026-03-06 11:11:55 -0500

    Add structure for function group index pages
    These pages will list composables and modifiers across all packages. This sets up the HTML page structure.
    
    Bug: 485897870
    Test: DefaultFunctionGroupIndexTest
    Change-Id: I6559a9ed342c681efd623626cc16e55347698a34

---
+++ src/test/java/com/google/devsite/components/impl/DefaultFunctionGroupIndexTest.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.devsite.components.impl
+
+import com.google.common.truth.Truth.assertThat
+import com.google.devsite.components.pages.FunctionGroupIndex.Params
+import com.google.devsite.components.testing.NoopSummaryList
+import kotlinx.html.body
+import kotlinx.html.stream.createHTML
+import org.junit.Test
+
+class DefaultFunctionGroupIndexTest {
+    @Test
+    fun `Function group index renders correctly`() {
+        val component = DefaultFunctionGroupIndex(Params(NoopSummaryList()))
+
+        val output = createHTML().body { component.render(this) }.trim()
+
+        // language=html
+        assertThat(output)
+            .isEqualTo(
+                """
+<body>
+  <div>noop</div>
+</body>
+            """
+                    .trim()
+            )
+    }
+}
