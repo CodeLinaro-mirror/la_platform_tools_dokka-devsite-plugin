4771bf44385ce7927a7d1aa50925a4dc3c368975
Julia McClellan <juliamcclellan@google.com>
2026-06-23 10:14:24 -0400

    Force dackka to exit after rendering error
    Currently, if there is an exception during rendering, dackka does not exit because dokka is not cleaning up the analysis API. This forces the process to exit until we get a fix in for that issue.
    
    Bug: 523326990
    Test: Ran `./gradlew :integration-test:cli:run` with an exception added in `ClasslikeDocumentableConverter`. Verified that without this change, the task didn't terminate after the error, but with this change it does.
    Change-Id: Ib4e2c83a688b31becd61cd31e150a91e83a411dc

--- src/main/java/com/google/devsite/renderer/MultiLanguageRenderer.kt
+++ src/main/java/com/google/devsite/renderer/MultiLanguageRenderer.kt
@@ -38,6 +38,7 @@ import com.google.devsite.util.JsonLibraryMetadata
 import com.google.devsite.util.JsonVersionMetadata
 import com.google.devsite.util.LibraryMetadata
 import com.google.devsite.util.traceCoroutine
+import kotlin.system.exitProcess
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -60,13 +61,21 @@ internal class MultiLanguageRenderer(
 ) : Renderer {
 
     override fun render(root: RootPageNode) {
-        context(devsiteConfiguration.tracer) {
-            render(
-                (root as ModulePageNode).documentables.single() as DModule,
-                DefaultExternalDokkaLocationProvider(
-                    dokkaLocationProvider = DokkaLocationProvider(root, context)
-                ),
-            )
+        try {
+            context(devsiteConfiguration.tracer) {
+                render(
+                    (root as ModulePageNode).documentables.single() as DModule,
+                    DefaultExternalDokkaLocationProvider(
+                        dokkaLocationProvider = DokkaLocationProvider(root, context)
+                    ),
+                )
+            }
+        } catch (e: Exception) {
+            // Workaround for https://github.com/Kotlin/dokka/issues/4537 -- when there is an
+            // exception during rendering, dokka doesn't dispose of all analysis API resources,
+            // which prevents the process from exiting. This can be removed once the issue is fixed.
+            e.printStackTrace()
+            exitProcess(1)
         }
     }
 
