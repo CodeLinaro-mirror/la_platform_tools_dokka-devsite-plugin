bd7602addd210e14c77ed256f6ac993f1696992a
Julia McClellan <juliamcclellan@google.com>
2026-08-03 15:41:49 -0400

    Bump dackka version to 1.10.4
    The new version upgrades dokka from 2.2.0-dev-415 to 2.3.0-dev-434
    
    Bug: 542261491
    Test: ./gradlew
    Change-Id: I098c1a4c46231de70c4ed05acb1ba402f6e21ff6

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ defaultTasks = mutableListOf("test", "jar", "shadowJar", "ktCheck", "publish", "
 
 group = "com.google.devsite"
 
-version = "1.10.3" // This is appended to archiveBaseName in the ShadowJar task.
+version = "1.10.4" // This is appended to archiveBaseName in the ShadowJar task.
 
 plugins {
     kotlin("jvm")
